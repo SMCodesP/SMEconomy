aa79ec13859333da6e1baa009e1fc27e6a9c5242
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-04-21 23:11:25 -0300

    Init project web

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início - SpetacularNetwork</title>
+      </Head>
+
+      <section>
+        <h1>Teste</h1>
+      </section>
+    </>
+  );
+}
