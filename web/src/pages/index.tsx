f099ecba71445b007ea74bf8f6be913b89876d27
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-04-22 00:05:57 -0300

    Login screen

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,15 +1,39 @@
 import Head from 'next/head';
 
+import { FaUser, FaLock } from 'react-icons/fa';
+
+import styles from '@/styles/Home.module.css';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início - SpetacularNetwork</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section>
-        <h1>Teste</h1>
-      </section>
+      <main className={styles.container}>
+        <section className={styles.containerLogin}>
+          <h1>Acesse sua conta</h1>
+          <div className={styles.containerInput}>
+            <FaUser size={28} color="#7A899A" />
+            <input
+              type="text"
+              name="username"
+              placeholder="Digite seu nickname"
+            />
+          </div>
+          <div className={styles.containerInput}>
+            <FaLock size={28} color="#7A899A" />
+            <input
+              type="password"
+              name="username"
+              placeholder="Digite sua senha"
+            />
+          </div>
+          <button className={styles.button}>Entrar</button>
+        </section>
+      </main>
     </>
   );
 }
