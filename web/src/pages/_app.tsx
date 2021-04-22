c38d9b99e7fab14ccefb54960c94542ba9b39595
SMCodesP <samuel.silva513@etec.sp.gov.br>
2021-04-22 17:08:06 -0300

    Add external font and more styling

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import Head from 'next/head';
 import { AppProps } from 'next/app';
 import '@/styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head';
 
-import { FaUser, FaLock } from 'react-icons/fa';
+import { FaUser, FaLock, FaDiscord, FaGithub } from 'react-icons/fa';
+import { GoogleFonts } from 'next-google-fonts';
 
 import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" />
       <Head>
         <title>Início - SpetacularNetwork</title>
         <link rel="icon" href="/favicon.ico" />
@@ -33,6 +35,18 @@ export default function Home() {
           </div>
           <button className={styles.button}>Entrar</button>
         </section>
+        <div className={styles.containerSocial}>
+          <a href="https://github.com/SMCodesP/SMEconomy" target="_blank">
+            <FaGithub size={32} color="#191622" />
+          </a>
+          <hr />
+          <a
+            href="https://discord.com/users/520311747098312725"
+            target="_blank"
+          >
+            <FaDiscord size={32} color="#191622" />
+          </a>
+        </div>
       </main>
     </>
   );
